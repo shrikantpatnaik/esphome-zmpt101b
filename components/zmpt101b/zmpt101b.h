7535cce93b44dddeadb7d749f8623e14a747c34d
Shrikant Patnaik <shrikant.patnaik@gmail.com>
2026-01-30 22:07:19 +0530

    Add missing include

--- components/zmpt101b/zmpt101b.h
+++ components/zmpt101b/zmpt101b.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/voltage_sampler/voltage_sampler.h"
 
 namespace esphome {
